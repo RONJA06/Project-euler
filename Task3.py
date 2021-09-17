662c70fb9e7e445dde553b196ba07fa12ac0f604
RONJA06 <81020692+RONJA06@users.noreply.github.com>
2021-09-17 10:06:29 +0300

    Task3 complite

--- Task3.py
+++ Task3.py
@@ -2,7 +2,7 @@ num = 600851475143
 i = 1
 while num > 1:
     i += 1
-    if i % 2 != 0 and (i % 3 != 0 and i > 3) and (i % 5 != 0 and i > 5) and (i % 7 != 0 and i > 7):
+    if (i % 2 != 0 or i == 2) and (i % 3 != 0 or i == 3) and (i % 5 != 0 or i == 5) and (i % 7 != 0 or i == 7):
         if num % i == 0:
             num /= i
 print(i)
